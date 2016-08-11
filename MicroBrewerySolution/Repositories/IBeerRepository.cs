32f021620ff0d7e9ff8392dfc125b71693ea5883
Knut <knusan@stud.ntnu.no>
2016-08-11 05:34:42 -0700

    Interfaces created

--- MicroBrewerySolution/Repositories/IBeerRepository.cs
+++ MicroBrewerySolution/Repositories/IBeerRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MicroBreweryLibrary;
 namespace Repositories
 {
-    interface IBeerRepository
+    public interface IBeerRepository
     {
+        void Add(Beer beer);
+        IEnumerable<Beer> GetAll();
     }
+
+
 }
